95899ea59e4a9892ee72a44cbbcc48679f0dc70d
camshaft <bytheway.cameron@gmail.com>
2015-04-01 23:59:44 -0600

    fix warnings in get

--- get.js
+++ get.js
@@ -6,17 +6,27 @@ var React = require('react');
 var subscribe = require('./registry').subscribe;
 
 var Get = React.createClass({
-  componentWillMount: function() {
+  componentDidMount: function() {
     this.subscription = subscribe(this.props.name, this.onChange);
   },
+  getInitialState: function() {
+    return {
+      content: null
+    };
+  },
   onChange: function(content) {
-    this.setState({content: content});
+    var self = this;
+    // wrap in a set timeout so we don't get warnings about setting state
+    // inside of a render function
+    setTimeout(function() {
+      self.setState({content: content});
+    });
   },
   componentWillUnmount: function() {
     this.subscription();
   },
   render: function() {
-    return this.state.content;
+    return this.state.content || false;
   }
 });
 
