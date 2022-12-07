6e18e7e15cabcdfd01a21db848b1c95a6dfb547c
Jake Richan <jake.richan@divvypay.com>
2022-12-07 12:12:37 -0500

    ci: add cz, changelog, and ci publish

--- libs/onus-react/src/lib/GetElement/index.tsx
+++ libs/onus-react/src/lib/GetElement/index.tsx
@@ -1,21 +1,24 @@
-import { useState, useEffect, useContext } from 'react'
-import { Context } from '../OnusElementsProvider'
-import { GetElementProps } from '../../types'
+import { useState, useEffect, useContext } from 'react';
+import { Context } from '../OnusElementsProvider';
+import { GetElementProps } from '../../types';
 
 const GetElement = ({ name, children = null }: GetElementProps) => {
-  const { subscribe } = useContext(Context)
-  const [content = null, setContent] = useState(null)
+  const { subscribe } = useContext(Context);
+  const [content = null, setContent] = useState(null);
   if (!subscribe) {
-    console.error('Onus Elements context not found. `OnusElementsProvider` is required', name)
+    console.error(
+      'Onus Elements context not found. `OnusElementsProvider` is required',
+      name
+    );
   }
 
   useEffect(() => {
-    if (!subscribe || !name) return
-    subscribe(name, setContent)
-  }, [name, subscribe])
+    if (!subscribe || !name) return;
+    subscribe(name, setContent);
+  }, [name, subscribe]);
 
   // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{content || children}</>
-}
+  return <>{content || children}</>;
+};
 
-export default GetElement
+export default GetElement;
--- libs/onus-react/src/lib/SetElement/index.tsx
+++ libs/onus-react/src/lib/SetElement/index.tsx
@@ -1,22 +1,31 @@
-import { useEffect, useContext } from 'react'
-import { Context } from '../OnusElementsProvider'
-import { getLocation } from '../utils'
-import { SetElementProps } from '../../types'
+import { useEffect, useContext } from 'react';
+import { Context } from '../OnusElementsProvider';
+import { getLocation } from '../utils';
+import { SetElementProps } from '../../types';
 
-const SetElement = ({ children = null, prepend, append, priority, name }: SetElementProps) => {
-  const location = getLocation({ append, prepend })
-  const { register, unregister } = useContext(Context)
+const SetElement = ({
+  children = null,
+  prepend,
+  append,
+  priority,
+  name,
+}: SetElementProps) => {
+  const location = getLocation({ append, prepend });
+  const { register, unregister } = useContext(Context);
 
-  if (!register) console.error('Onus Elements context not found. `OnusElementsProvider` is required')
+  if (!register)
+    console.error(
+      'Onus Elements context not found. `OnusElementsProvider` is required'
+    );
 
   useEffect(() => {
-    if (!register) return
-    register({ children, name, priority }, location)
+    if (!register) return;
+    register({ children, name, priority }, location);
     return () => {
-      unregister(name, priority)
-    }
-  }, [children, location, name, priority, register, unregister])
-  return null
-}
+      unregister(name, priority);
+    };
+  }, [children, location, name, priority, register, unregister]);
+  return null;
+};
 
-export default SetElement
+export default SetElement;
