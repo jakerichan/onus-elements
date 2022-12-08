d42966e3bd88c91ec988d65d0ba0e12ce9437f3d
Jake Richan <jake.richan@divvypay.com>
2022-12-08 14:39:19 -0500

    ci: adding lint-staged to husky pre-commit

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '{libs,apps,tools}/**/*.{ts,js,json,md,html,css,scss}': [
+    'npx nx affected --target lint --uncommitted --fix true',
+    'npx nx affected --target test --uncommitted',
+    'npx nx format:write --uncommitted',
+  ]
+}
