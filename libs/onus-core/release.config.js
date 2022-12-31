a5fa81e1c4f1f8cde5ee0dc19674f8d677139b78
Jake Richan <jake.richan@divvypay.com>
2022-12-30 21:28:44 -0500

    fix(onus-core): implement release target so core can be published

---
+++ libs/onus-core/release.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-template-curly-in-string */
+
+const name = '@onus-elements/core';
+const srcRoot = 'libs/onus-core';
+
+module.exports = {
+  extends: 'release.config.base.js',
+  branches: ['main'],
+  pkgRoot: `dist/${srcRoot}`,
+  tagFormat: name + '-v${version}',
+  commitPaths: [`${srcRoot}/*`],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `${srcRoot}/CHANGELOG.md`,
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [`${srcRoot}/package.json`, `${srcRoot}/CHANGLOG.md`],
+        message:
+          `release(version): Release ${name} ` +
+          '${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
